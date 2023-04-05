e17e6b6d0f0c5d596a14b1abda2a8d68f3ec3894
ayseekinerdur <ekinerdurr@gmail.com>
2023-04-05 18:29:09 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// This is comment
+
+string myName = "Ekin";
+string myLastName = "Erdur";
+
+//Console.WriteLine( "Hello" + " " + myName);
+ 
+ Console.WriteLine($"Hello, {myName}  {myLastName}! ");
